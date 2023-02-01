28d588f89debd499ffd9a652ff4eafbc62068d7e
MrOmarxD <omareiyana@gmail.com>
2023-02-01 12:29:12 +0100

    subir proyecto

---
+++ Sustitos/Proyecto/src/main/java/beans/Proveedor.java
@@ -0,0 +1,41 @@
+package beans;
+
+public class Proveedor {
+	private String nombre;
+    private String encargado;
+    private String categoria;
+    private int telefono;
+    private String email;
+    
+    // get / set
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEncargado() {
+		return encargado;
+	}
+	public void setEncargado(String encargado) {
+		this.encargado = encargado;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
